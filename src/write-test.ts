7175adb3803c044b2edeb328a54d6197df33a5ab
ShMcK <shawn.j.mckay@gmail.com>
2016-08-01 14:56:29 -0700

    compile content with TS compiler

--- src/write-test.ts
+++ src/write-test.ts
@@ -2,11 +2,14 @@ import {writeFile} from 'fs';
 import {logger} from 'process-console-log';
 import exists from './exists';
 import {testPath} from './constants';
+import * as ts from 'typescript';
 
 export default function writeTest(config, testString: string) {
   return new Promise((resolve, reject) => {
-		// append logger
-    const output = logger + exists(config.dir) + testString;
+		// append logger, compile using ts compiler
+    const output = logger + exists(config.dir) + ts.transpile(testString, {
+      module: ts.ModuleKind.CommonJS
+    });
 		// write test file
     writeFile(testPath, output, (err) => {
       if (err) { reject(err); }
