b24588696f30d8974e536df3eda1556b4fab356f
ShMcK <shawn.j.mckay@gmail.com>
2016-08-07 20:34:53 -0700

    fix runner inputs

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,7 +1,7 @@
 import startRunner from './start-runner';
 import spawnRunnerProcess from './runner-process';
 
-export default function runner(options) {
-    const runner = spawnRunnerProcess(options);
-    return startRunner(runner, options.handleResult);
+export default function runner({dir, taskPosition, handleResult, testPath}) {
+    const runner = spawnRunnerProcess({dir, taskPosition, testPath});
+    return startRunner({runner, handleResult, taskPosition});
 }
--- src/writeTests/helpers/babel-register.ts
+++ src/writeTests/helpers/babel-register.ts
@@ -1,2 +1,2 @@
-const babelRegister = `require("babel-register")({"plugins": [["transform-es2015-modules-commonjs", {"strict": true,"loose": true}]]});\n\n`;
+const babelRegister = `require("babel-register")({"plugins": [["transform-es2015-modules-commonjs",{"strict":true,"loose":true}]]});\n\n`;
 export default babelRegister;
