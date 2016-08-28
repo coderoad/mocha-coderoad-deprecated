4fc613c2a8907026782151caeac95197503de2a8
ShMcK <shawn.j.mckay@gmail.com>
2016-08-28 16:30:45 -0700

    update for windows

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,7 +1,15 @@
 import startRunner from './start-runner';
 import spawnRunnerProcess from './runner-process';
 
+const isWindows = window.navigator.appVersion.indexOf('Win') > -1;
+
 export default function runner({dir, taskPosition, handleResult, testPath}) {
-    const runner = spawnRunnerProcess({dir, taskPosition, testPath});
-    return startRunner({runner, handleResult, taskPosition});
+
+  if (isWindows) {
+    testPath = testPath.split('\\').join('\\\\');
+    testPath = testPath.split('/').join('\\\\');
+  }
+
+  const runner = spawnRunnerProcess({dir, taskPosition, testPath});
+  return startRunner({runner, handleResult, taskPosition});
 }
--- src/testPath.ts
+++ src/testPath.ts
@@ -1,10 +1,9 @@
 import { join } from 'path';
 
-const isWindows = window.navigator.appVersion.indexOf('Win') > -1 || false;
+const isWindows = window.navigator.appVersion.includes('Win') || false;
 
 export default function tmpTestName(testFile: string): string {
   let testPath = join(__dirname, '..', '.tmp', testFile + '.js');
-  // fix bug on Windows for test paths
   if (isWindows) {
     testPath = testPath.split('/').join('\\');
   }
